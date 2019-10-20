0f9386436c084971ec390d52980019c5397dd541
Monica Marziano <mmarziano1@gmail.com>
2019-10-20 19:13:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function mondayWork(activity="go to the office") {
 
 function wrapAdjective(flair='*') {
   return function(param="special") {
-    return `You are ${param}!`
+    return `You are ${flair}${param}${flair}!`
   }
 }
